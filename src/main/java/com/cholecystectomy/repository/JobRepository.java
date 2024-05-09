ecd5b7e25a74adc1f34ebd672354a90ff8e3c80d
yvnlarrye <eugenymc07@gmail.com>
2024-05-09 19:06:34 +0300

    removed unused imports

--- src/main/java/com/cholecystectomy/repository/JobRepository.java
+++ src/main/java/com/cholecystectomy/repository/JobRepository.java
@@ -4,7 +4,6 @@ import com.cholecystectomy.domain.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 
 @Repository
