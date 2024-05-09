4833a203b3f2a344fa9c256512bdd0e3f3afe8f2
yvnlarrye <eugenymc07@gmail.com>
2024-05-09 19:09:50 +0300

    refactored and implemented new endpoints handlers

--- src/main/java/com/cholecystectomy/controller/GeneralController.java
+++ src/main/java/com/cholecystectomy/controller/GeneralController.java
@@ -3,23 +3,23 @@ package com.cholecystectomy.controller;
 import com.cholecystectomy.domain.model.User;
 import com.cholecystectomy.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-
 @RestController
-@RequestMapping("/api/v1/test")
+@RequestMapping("api/v1")
 @RequiredArgsConstructor
-public class TestController {
+public class GeneralController {
 
     private final UserService userService;
 
-    @GetMapping("/test-endpoint")
-    public ResponseEntity<String> getAllUsers() {
-        return ResponseEntity.ok("Hello, this is secured zone");
+    @GetMapping("/user-info")
+    public ResponseEntity<User> getUserInfo() {
+        return new ResponseEntity<>(userService.getCurrentUser(), new HttpHeaders(), HttpStatus.OK);
     }
+
 }
