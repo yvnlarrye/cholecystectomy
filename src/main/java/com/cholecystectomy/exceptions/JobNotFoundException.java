c5f9634119f124580cdc4903f85a7e652cb24f65
yvnlarrye <eugenymc07@gmail.com>
2024-05-09 18:59:43 +0300

    removed some not unnecessary classes

--- src/main/java/com/cholecystectomy/exceptions/JobNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.cholecystectomy.exceptions;
-
-public class JobNotFoundException extends RuntimeException {
-    private static final String MESSAGE = "Должность не найдена";
-    public JobNotFoundException() {
-        super(MESSAGE);
-    }
-}
