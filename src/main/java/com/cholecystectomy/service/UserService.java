602b64868f9f99ab48dc0eaa8710f6cecc3f7f7c
yvnlarrye <eugenymc07@gmail.com>
2024-05-09 19:05:33 +0300

    adapted UserService to new entity inheritance strategy

--- src/main/java/com/cholecystectomy/service/UserService.java
+++ src/main/java/com/cholecystectomy/service/UserService.java
@@ -1,16 +1,12 @@
 package com.cholecystectomy.service;
 
 import com.cholecystectomy.domain.model.User;
-import com.cholecystectomy.domain.model.Role;
 import com.cholecystectomy.repository.UserRepository;
-import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,38 +14,13 @@ public class UserService {
     private final UserRepository repository;
 
     /**
-     * Сохранение пользователя
-     *
-     * @return сохраненный пользователь
-     */
-    public User save(User user) {
-        return repository.save(user);
-    }
-
-
-    /**
-     * Создание пользователя
-     *
-     * @return созданный пользователь
-     */
-    public User create(User user) {
-
-        if (repository.existsByEmail(user.getUsername())) {
-            throw new EntityExistsException("Пользователь с таким email уже существует");
-        }
-
-        return save(user);
-    }
-
-    /**
      * Получение пользователя по имени пользователя
      *
      * @return пользователь
      */
-    public User getByUsername(String email) {
+    public User getByEmail(String email) {
         return repository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
-
     }
 
     /**
@@ -60,7 +31,7 @@ public class UserService {
      * @return пользователь
      */
     public UserDetailsService userDetailsService() {
-        return this::getByUsername;
+        return this::getByEmail;
     }
 
     /**
@@ -70,8 +41,12 @@ public class UserService {
      */
     public User getCurrentUser() {
         // Получение имени пользователя из контекста Spring Security
-        var username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return getByUsername(username);
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getByEmail(email);
+    }
+
+    public void deleteUser(User user) {
+        repository.delete(user);
     }
 
 }
