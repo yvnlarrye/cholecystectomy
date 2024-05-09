4528e5af7d7e22b71d32034e7671744870924c4f
yvnlarrye <eugenymc07@gmail.com>
2024-05-09 19:07:03 +0300

    added new patients fetch method

--- src/main/java/com/cholecystectomy/repository/PatientRepository.java
+++ src/main/java/com/cholecystectomy/repository/PatientRepository.java
@@ -2,8 +2,14 @@ package com.cholecystectomy.repository;
 
 import com.cholecystectomy.domain.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    @Query("SELECT p FROM Patient p WHERE p.doctor_id = NULL")
+    List<Patient> findAllPatientsWithNoDoctor();
 }
